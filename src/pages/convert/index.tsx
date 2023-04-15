f01f5f5d6ba78df55746c219e5c9c0eb001f4ce9
Thiago <101950809+Azbito@users.noreply.github.com>
2023-04-15 18:24:22 -0300

    CHORE: New page coin converter

--- src/pages/convert/index.tsx
+++ src/pages/convert/index.tsx
@@ -1,50 +1,117 @@
-import { styles } from "@/styles/styles"
-import { FormControlLabel, Radio, RadioGroup } from "@mui/material"
+import { FormControl, FormControlLabel, FormHelperText, FormLabel, Grow, InputLabel, List, MenuItem, Radio, RadioGroup, Select, TextField } from "@mui/material"
+
 import axios from "axios"
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { styles } from "..";
+import world from '../../../public/images/world.gif'
+import Image from "next/image";
 
 export default function Convert() {
-  const [coinValue, setCoinValue] = useState({
-    ticker: {
-      buy: ""
-    }
-  })
+  const [coinValue, setCoinValue] = useState(null);
+  const [coinQuotationValue, setCoinQuotationValue] = useState(null);
+  const [convertCoin, setConvertCoin] = useState('');
+  const [convertQuotationCoin, setConvertQuotationCoin] = useState('');
+  const [convertableCoin, setConvertableCoin] = useState(0);
 
-  const [coin, setCoin] = useState('');
   const handleCoinChange = (event: any) => {
-    setCoin(event.target.value);
-    console.log(coin)
+    setConvertQuotationCoin(event.target.value);
   };
+  const [allCoins, setAllCoins] = useState(null);
+  const [result, setResult] = useState<number | null>(null)
+
+
   async function Quotation() {
-    if (coin === "USD") {
-      try {
-        const { data } = await axios.get("https://www.mercadobitcoin.net/api/USDC/ticker")
-        setCoinValue(data)
-        console.log(coinValue)
-        return;
-      } catch (error) {
-        alert(error)
-        return;
-      }
+    try {
+      const { data } = await axios.get(`https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL`)
+      setAllCoins(data)
+    } catch (error) {
+      alert("It couldn't find any coin. Please, try it again.")
     }
   }
+
+  useEffect(() => {
+    Quotation()
+  }, [])
+
+  useEffect(() => {
+    if (allCoins != null) {
+      const selectedCoin: any = Object.entries(allCoins).find((item: any) => item[0] == convertCoin)
+      setCoinValue(selectedCoin[1].ask)
+    }
+  }, [convertCoin])
+
+  useEffect(() => {
+    if (allCoins != null) {
+      const selectedCoinQuotation: any = Object.entries(allCoins).find((item: any) => item[0] == convertQuotationCoin)
+      setCoinQuotationValue(selectedCoinQuotation[1].ask)
+    }
+
+  }, [convertQuotationCoin])
+
+  function Convert() {
+    const calc: number = convertableCoin * Number(coinValue)
+    setResult(calc)
+  }
+
   return (
-    <div className={styles.containerConvert}>
-      <div className={styles.welcome}>
-        <h1 className={styles.title}>
-          See the quotation today!
-        </h1>
+    <div className="mb-32">
+      <div className={styles.containerConvert}>
+        <div className={styles.welcome}>
+          <h1 className={styles.title}>
+            See the quotation today!
+          </h1>
+        </div>
+        <div>
+          <h1 className={styles.coinValue}>{(Number(coinQuotationValue)).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h1>
+        </div>
+        <div className={styles.selectDiv}>
+          <FormControl>
+            <InputLabel id="demo-simple-select-label">Coin</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={convertQuotationCoin}
+              label="Coin"
+              onChange={handleCoinChange}
+              className="w-32"
+            >
+              <MenuItem value="USDBRL">USD</MenuItem>
+              <MenuItem value="EURBRL">EUR</MenuItem>
+              <MenuItem value="BTCBRL">BTC</MenuItem>
+            </Select>
+          </FormControl>
+        </div>
+        <Image className={styles.animationConvertor} src={world} alt="World" />
+        <div className={styles.welcome}>
+          <h1 className={styles.title}>
+            Convert
+          </h1>
+        </div>
+        <div className="mt-[2rem] flex flex-col items-center justify-center">
+          <TextField inputProps={{ inputMode: 'numeric' }} id="outlined-basic" label="Type here the value" variant="outlined" value={convertableCoin} onChange={(e: any) => setConvertableCoin(e.target.value)} />
+          <FormControl>
+            <RadioGroup
+              aria-labelledby="demo-radio-buttons-group-label"
+              defaultValue="USD"
+              name="radio-buttons-group"
+              value={convertCoin}
+              onChange={(e) => setConvertCoin(e.target.value)}
+            >
+              <FormControlLabel value="USDBRL" control={<Radio />} label="USD" />
+              <FormControlLabel value="EURBRL" control={<Radio />} label="EUR" />
+              <FormControlLabel value="BTCBRL" control={<Radio />} label="BTC" />
+            </RadioGroup>
+          </FormControl>
+          {result && result > 0 ?
+            <Grow in>
+              <h1 className="bg-lime-800 p-1 rounded-sm text-white mt-5">{result?.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}</h1>
+            </Grow>
+            :
+            <></>
+          }
+          <button className={styles.button} onClick={Convert}>Get Quotation</button>
+        </div>
       </div>
-      <div>
-        <h1>{(Number(coinValue.ticker.buy)).toFixed(2)}</h1>
-      </div>
-      <div className={styles.radioDiv} >
-        <RadioGroup aria-label="Money" name="gender" value={coin} onChange={handleCoinChange}>
-          <FormControlLabel className={styles.radioDiv} value="USD" control={<Radio />} label="USD" />
-          <FormControlLabel className={styles.radioDiv} value="EUR" control={<Radio />} label="EUR" />
-        </RadioGroup>
-      </div>
-      <button className={styles.button} onClick={Quotation}>Convert</button>
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,26 @@
 import { Grow } from '@mui/material'
 import Image from 'next/image'
-import Investment from '../../public/images/investment.gif'
+import Crisis from '../../public/images/crisis.gif'
 
 import { useRouter } from 'next/router'
-import { styles } from '@/styles/styles'
+
+export const styles = {
+  container: 'flex items-center justify-center flex-col',
+  containerConvert: 'flex items-center justify-center flex-col mt-[10rem]',
+  content: 'flex items-center text-center justify-center flex-col mt-[2rem]',
+  welcome:
+    'bg-emerald-700 p-5 rounded-lg flex items-center justify-center flex-col shadow-2xl mt-[2rem]',
+  webTitle: 'text-emerald-500 font-bold text-4xl mt-[10rem]',
+  title: 'text-white text-2xl font-bold',
+  description: 'text-white italic text-center',
+  animation: 'w-[20rem] mr-[60rem] absolute',
+  button:
+    'bg-lime-600 p-3 text-white rounded-lg hover:bg-lime-500 duration-300 active:bg-lime-700 mt-[2rem]',
+  selectDiv: 'mt-[2rem] z-10',
+  coinValue: 'bg-indigo-500 mt-[2rem] p-2 rounded-sm text-white',
+  animationConvertor: "w-[20rem] ml-[60rem] absolute"
+}
+
 
 export default function Home() {
   const router = useRouter()
@@ -19,7 +36,7 @@ export default function Home() {
           </div>
         </Grow>
         <button className={styles.button} onClick={() => router.push('/convert')}>Convert</button>
-        <Image className={styles.animation} src={Investment} alt="Investment" />
+        <Image className={styles.animation} src={Crisis} alt="Crisis" />
       </div>
     </div>
   )
